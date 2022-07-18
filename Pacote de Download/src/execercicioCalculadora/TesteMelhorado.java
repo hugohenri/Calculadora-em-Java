2e70fbd510c2d1afd903313fff7a0c9a281091f4
Hugo Henrique <hugohenrisoares@gmail.com>
2022-07-17 21:19:21 -0300

    Laço de repetição nas opções iniciais

--- Pacote de Download/src/execercicioCalculadora/TesteMelhorado.java
+++ Pacote de Download/src/execercicioCalculadora/TesteMelhorado.java
@@ -6,9 +6,19 @@ public class TesteMelhorado {
     public static void main(String[] args) throws Exception {
         
         Calculadora calculadora = new Calculadora();
+        int sair=0;
+
+        do{
 
         String opcao = JOptionPane.showInputDialog(null, "1- SOMA \n2- SUBTRAÇÃO\n3- MULTIPLICAÇÃO\n4- DIVISÃO");
+        int opcaoNum = Integer.parseInt(opcao);
         
+            
+        if((opcaoNum >=1) && (opcaoNum <=4) ){
+
+        
+      
+       
         String numero1 = JOptionPane.showInputDialog(null, "Digite o primeiro numero");
         String numero2 = JOptionPane.showInputDialog(null, "Digite o segundo numero");
         calculadora.num1 = Float.parseFloat(numero1);
@@ -42,9 +52,15 @@ public class TesteMelhorado {
         if(calculadora.alert != 0){
         JOptionPane.showMessageDialog(null, "O resulto é: " + calculadora.res, "Calculadora", JOptionPane.INFORMATION_MESSAGE);
         }
+        sair =1;
        
         
-        
+    }
+    else{
+        JOptionPane.showMessageDialog(null, "ERRO", "Calculadora", JOptionPane.ERROR_MESSAGE);
+    }
+
+}while(sair != 1);
         
         
     }
